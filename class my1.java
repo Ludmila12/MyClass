27b13d18f8edba89ffffae4f5df953173eb1e120
Ludmila12 <37815443+Ludmila12@users.noreply.github.com>
2018-04-05 17:20:52 +0300

    Create class my1.java

---
+++ class my1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class my1 {
+    public static void main(String[] args) {
+        Scanner sc2 = new Scanner(System.in);
+        int n = sc2.nextInt();
+        int[] a = new int[n];
+
+        int b = a[0];
+        a[0]=a[n-1];
+        a[n-1]=b;
+        String s =Arrays.toString(a);
+        System.out.println(s);
+    }
+}
